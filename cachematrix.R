e0deb52f7fbacf7b8d097fce59c66d34d8d64506
edwald <tryggvi.edwald@gmail.com>
2014-05-22 22:43:49 +0200

    Added untested R file

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,34 @@
-## Put comments here that give an overall description of what your
-## functions do
+## These functions set up matrix objects that hold their own inverses, if known,
+## and return them 'from memory' rather than re-calculate if needed more than once.
 
-## Write a short comment describing this function
+## Set up a matrix object which can hold its own inverse if known
 
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function(x = numeric()) {
+	i   <- NULL
+	set <- function(y) {
+		x <<- y
+		i <<-NULL
+	}
+	get <- function() x
+	setinv <- function(ing) i <<- ing
+	getinv <- function() i
+	list( set=set, get=get, setinv = setinv, getinv = getinv )
 }
 
 
-## Write a short comment describing this function
+## return the inverse of a matrix (assumed to be invertible) either by calculating
+#  or by recalling 'from memory' if we've done this before
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+	i <- x$getinv()
+	if( !is.null(i)) {
+		message("getting cached data")
+		return(i)
+	}
+	d <- x$get()
+	i <- solve(d)
+	x$setinv(i)
+	i
 }
+
